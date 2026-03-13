304d68fe7861e0bb007724bf92e808eb602bb443
mostafakamal90-ship-it <mostafa.kamal90@gmail.com>
2026-03-13 21:51:20 +0200

    Create universe_buildder.py

---
+++ universe_buildder.py
@@ -0,0 +1,10 @@
+import yfinance as yf
+
+def build_universe():
+
+    tickers = [
+        "AAPL","MSFT","GOOG","AMZN","META",
+        "NVDA","AMD","TSLA","INTC","NFLX"
+    ]
+
+    return tickers
